ac2788a506859582a96ba694a647adfe91103969
BABYdotRAR <lopezoscar0101lp@gmail.com>
2025-08-15 18:03:47 -0600

    refactor: simple tweaks to make code cleaner

--- data_structures/stack.go
+++ data_structures/stack.go
@@ -2,43 +2,54 @@ package ds
 
 import "sync"
 
+// NewStack retrieves a new generic stack
+func NewStack[T any]() *Stack[T] {
+	return &Stack[T]{}
+}
+
 // Stack (thread-safe) is a generic LIFO stack
 type Stack[T any] struct {
 	lock sync.Mutex
 	size int
-	top *node[T]
+	top  *node[T]
 }
 
 type node[T any] struct {
 	value T
-	next *node[T]
+	next  *node[T]
 }
 
 // IsEmpty returns whether the stack contains no elements
-func (s *Stack[T])IsEmpty() bool {
+func (s *Stack[T]) IsEmpty() bool {
 	s.lock.Lock()
 	defer s.lock.Unlock()
 
+	return s.isEmpty()
+}
+
+func (s *Stack[T]) isEmpty() bool {
 	return s.top == nil
 }
 
 // Push adds elem at the top of the stack
-func (s *Stack[T])Push(elem T) {
+func (s *Stack[T]) Push(elem T) {
 	s.lock.Lock()
 	defer s.lock.Unlock()
 
-	newTop := newNode(s, elem)
-	s.top = &newTop
+	s.top = &node[T]{
+		value: elem,
+		next:  s.top,
+	}
 	s.size++
 }
 
 // Pop removes and returns the top element of the stack.
 // It returns the zero value of T and false if the stack is empty
-func (s *Stack[T])Pop() (T, bool) {
+func (s *Stack[T]) Pop() (T, bool) {
 	s.lock.Lock()
 	defer s.lock.Unlock()
 
-	if s.top == nil {
+	if s.isEmpty() {
 		var zeroValue T
 		return zeroValue, false
 	}
@@ -55,7 +66,7 @@ func (s *Stack[T]) Top() (T, bool) {
 	s.lock.Lock()
 	defer s.lock.Unlock()
 
-	if s.top == nil {
+	if s.isEmpty() {
 		var zeroValue T
 		return zeroValue, false
 	}
@@ -64,7 +75,7 @@ func (s *Stack[T]) Top() (T, bool) {
 
 // Size returns the current number of elements in the stack
 // O(1) cost
-func (s *Stack[T])Size() int {
+func (s *Stack[T]) Size() int {
 	s.lock.Lock()
 	defer s.lock.Unlock()
 
@@ -72,17 +83,10 @@ func (s *Stack[T])Size() int {
 }
 
 // Clear removes all elements from the stack
-func (s *Stack[T])Clear() {
+func (s *Stack[T]) Clear() {
 	s.lock.Lock()
 	defer s.lock.Unlock()
 
 	s.top = nil
 	s.size = 0
-}
-
-func newNode[T any](s *Stack[T], elem T) node[T] {
-	return node[T]{
-		value: elem,
-		next: s.top,
-	}
 }
