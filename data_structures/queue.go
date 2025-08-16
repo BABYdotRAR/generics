d705d2269abac8228f7bae85f4ecfdb79a0a9e59
BABYdotRAR <lopezoscar0101lp@gmail.com>
2025-08-15 18:02:01 -0600

    feat: finished queue implementation

--- data_structures/queue.go
+++ data_structures/queue.go
@@ -2,6 +2,12 @@ package ds
 
 import "sync"
 
+// NewQueue retrieves a new generic queue
+func NewQueue[T any]() *Queue[T] {
+	return &Queue[T]{}
+}
+
+// Queue (thread-safe) is a generic FIFO queue
 type Queue[T any] struct {
 	lock  sync.Mutex
 	size  int
@@ -9,40 +15,89 @@ type Queue[T any] struct {
 	rear  *node[T]
 }
 
+// Enqueue adds val to the end of the queue
 func (q *Queue[T]) Enqueue(val T) {
 	q.lock.Lock()
 	defer q.lock.Unlock()
 
-	q.size++
-	node := node[T]{
+	newNode := &node[T]{
 		value: val,
 		next:  nil,
 	}
 
-	if q.front == nil {
-		q.front = &node
-		q.rear = &node
+	if q.isEmpty() {
+		q.front, q.rear = newNode, newNode
+		q.size = 1
 		return
 	}
 
-	q.rear.next = &node
-	q.rear = &node
+	q.rear.next = newNode
+	q.rear = newNode
+	q.size++
 }
 
-func (q *Queue[T])Dequeue() (T, bool) {
+// Dequeue retrieves the first element in the queue and removes it. 
+// It returns the zero value for T and false if the queue is empty.
+func (q *Queue[T]) Dequeue() (T, bool) {
 	q.lock.Lock()
 	defer q.lock.Unlock()
 
-	if q.front == nil {
+	if q.isEmpty() {
 		var zeroValue T
 		return zeroValue, false
 	}
 
-	q.size--
-	if q.front == q.rear {
-		q.rear = nil
-	}
 	res := q.front
 	q.front = res.next
+	if q.isEmpty() {
+		q.rear = nil
+	}
+
+	q.size--
+	res.next = nil
+
 	return res.value, true
+}
+
+// Peek only retrieves the first element in the queue without removing it. 
+// It returns the zero value for T and false if the queue is empty.
+func (q *Queue[T])Peek() (T, bool) {
+	q.lock.Lock()
+	defer q.lock.Unlock()
+
+	if q.isEmpty() {
+		var zeroValue T
+		return zeroValue, false
+	}
+
+	return q.front.value, true
+}
+
+// Size returns the total number of elements in the queue
+func (q *Queue[T])Size() int {
+	q.lock.Lock()
+	defer q.lock.Unlock()
+
+	return q.size
+}
+
+// Clear resets the queue
+func (q *Queue[T])Clear() {
+	q.lock.Lock()
+	defer q.lock.Unlock()
+
+	q.front, q.rear = nil, nil
+	q.size = 0
+}
+
+// IsEmpty returns whether the queue contains no elements
+func (q *Queue[T])IsEmpty() bool {
+	q.lock.Lock()
+	defer q.lock.Unlock()
+
+	return q.isEmpty()
+}
+
+func (q *Queue[T])isEmpty() bool {
+	return q.front == nil
 }
